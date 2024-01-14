7a3a9d252c1cc8434d75991f67d9fc516905ebd0
pankaj04101986 <pankaj.lasurkar@gmail.com>
2024-01-14 16:26:36 +0530

    my subnets

---
+++ mysubnet.tf
@@ -0,0 +1,9 @@
+resource "aws_subnet" "public_sub" {
+  vpc_id = aws_vpc.MyVPC.id
+  cidr_block = "10.0.1.0/24"
+}
+
+resource "aws_subnet" "private_sub" {
+  vpc_id = aws_vpc.MyVPC
+  cidr_block = "10.0.2.0/24"
+}
